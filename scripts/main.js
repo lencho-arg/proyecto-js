3c0a3710ac5ae501ff3000a3f75434f925f38c2e
Lencina Federico <lencina_federicon@hotmail.com>
2022-06-24 00:29:05 -0300

    sweet

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,11 @@ import * as datos from './modeloDatos.js'
 document.addEventListener('DOMContentLoaded', function(){
 
     let cambiarBg = document.getElementById('fondo')
-    let color = ['black','linear-gradient(45deg, #6ea7fd, #69018f)'];
+    let color = ['black']//,'linear-gradient(45deg, #6ea7fd, #69018f)'];
 
     function btnChanger(){
-        let random = Math.floor(Math.random()*color.length)
-        let cambiarBg = document.getElementById('container1').style.background = color[random]
+        // let random = Math.floor(Math.random()*color.length)
+        cambiarBg = document.getElementById('container1').style.background = color//[random]
     }
 
     cambiarBg.addEventListener("click",()=>{
@@ -34,14 +34,25 @@ document.addEventListener('DOMContentLoaded', function(){
 
         let usuarioEncontrado = (datos.listaUsers).find((elemento)=>(elemento.user).includes(document.getElementById('formID')[0].value))
 
+
         if(usuarioEncontrado != undefined){
             let validacion = usuarioEncontrado.validar(document.getElementById('formID')[0].value, document.getElementById('formID')[1].value)
 
             if((document.getElementById('formID')[0].value) === '' || (document.getElementById('formID')[0].value) === null || (document.getElementById('formID')[0].value) === 'undefined'){
                 alert('El campo nombre no puede estar vacio')
+                // Swal.fire({
+                //     icon: 'error',
+                //     title: 'Oops...',
+                //     text: 'El campo nombre no puede estar vacio',
+                //   })
             }else{
                 if(document.getElementById('formID')[1].value === '' || document.getElementById('formID')[1].value === null || document.getElementById('formID')[1].value === 'undefined'){
                     alert('El campo password esta vacio')
+                    // Swal.fire({
+                    //     icon: 'error',
+                    //     title: 'Oops...',
+                    //     text: 'El campo password esta vacio',
+                    //   })
                 }
             }
 
@@ -54,27 +65,27 @@ document.addEventListener('DOMContentLoaded', function(){
                     title: 'Cambiaste el fondo',
                     showConfirmButton: false,
                     timer: 1500
+                }).then((result) =>{
+                    window.location.href="./recorridos.html"
                 })
-                
-                window.location.href="./recorridos.html"
 
-                // const enviar = ()=>{
-
-
-                    // boton1 = document.createElement('div')
-                    // boton1.innerHTML = '<div class="card text-center mt-5"><div class="card-header fw-semibold">Bienvenido</div><div class="card-body text-white bg-dark"><h5 class="card-title">BUSCHOICE</h5><p class="card-text">Sigamos viajando.</p><a href="../recorridos.html" class="btn btn-primary">Ir a los Recorridos</a></div></div>'
-                    
-                    // document.getElementById('btnEnviando').append(boton1)
-
-                // }
-
-                // boton1.addEventListener('mouseover',enviar)
+                // window.location.href="./recorridos.html"
       
             }else{
-                alert('Usuario y Contraseña Incorrectos')
+                // alert('Usuario y Contraseña Incorrectos')
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Usuario y Contraseña Incorrectos',
+                  })
             }
         }else{
-            alert('No te encontre en la base de datos')
+            // alert('No te encontre en la base de datos')
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'No te encontre en la base de datos',
+              })
             
         }
 
